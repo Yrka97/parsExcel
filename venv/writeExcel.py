f28fca228b97ea3bf26acbc0ef6c3cb48bb3f059
Cartmanidze <glebon84@gmail.com>
2018-01-08 13:35:02 +0600

    first commit
    Signed-off-by: Cartmanidze <glebon84@gmail.com>

---
+++ venv/writeExcel.py
@@ -0,0 +1,6 @@
+import xlrd
+rb = xlrd.open_workbook('d:/final.xls',formatting_info=True)
+sheet = rb.sheet_by_index(0)
+for rownum in range(sheet.nrows):
+    row = sheet.row_values(rownum)
+    print(row)
