c6f61ecfa77ecb9c726c9402efb657b0a8a92282
Cartmanidze <glebon84@gmail.com>
2018-01-08 16:08:40 +0600

    Parsing
    Signed-off-by: Cartmanidze <glebon84@gmail.com>

--- venv/writeExcel.py
+++ venv/writeExcel.py
@@ -1,17 +1,31 @@
 import xlrd
 class readExcel:
 
-    def readExcel(self,nameFileExcel):
+    def writeInfromToFile(self):
+        obj1.writeFile('some.txt','База знаний.')
+        obj1.writeFile('some.txt','\n')
+        obj1.writeFile('some.txt','Автор: Ожогин Юрий.')
+        obj1.writeFile('some.txt', '\n')
+        obj1.writeFile('some.txt', '\n')
+
+    def writeExcelQuestions(self,nameFileExcel):
         try:
             rb = xlrd.open_workbook(nameFileExcel)
         except FileNotFoundError:
             print('Ошибка файл не найден')
         sheet = rb.sheet_by_index(0)
-        for rownum in range(sheet.nrows):
-            row = sheet.row_values(rownum)
+        i = 1
+        obj1.writeFile('some.txt', 'Вопросы:')
+        obj1.writeFile('some.txt', '\n')
+        while i<len(sheet.row_values(1)):
+            row = sheet.row_values(1)[i]
             stringRow = str(row)
-            obj1.writeFile('some.txt',stringRow)
-            print(stringRow)
+            stringRow = stringRow.replace('[','')
+            stringRow = stringRow.replace(']','')
+            stringRow = stringRow.replace("'","")
+            obj1.writeFile('some.txt',stringRow+'?')
+            obj1.writeFile('some.txt', '\n')
+            i = i + 2
 
     def writeFile(self,nameFile,write):
         try:
@@ -23,5 +37,23 @@ class readExcel:
         except IOError:
             print('Ошибка записи в файл')
 
+    def writeOtherInf(self,nameFileExcel):
+        try:
+            rb = xlrd.open_workbook(nameFileExcel)
+        except FileNotFoundError:
+            print('Ошибка файл не найден')
+        sheet = rb.sheet_by_index(0)
+        for rownum in range(sheet.nrows):
+            row = sheet.row_values(rownum)
+            stringRow = str(row)
+            stringRow = stringRow.replace('[', '')
+            stringRow = stringRow.replace(']', '')
+            stringRow = stringRow.replace("'", "")
+            #obj1.writeFile('some.txt', stringRow)
+            #obj1.writeFile('some.txt', '\n')
+            print(stringRow)
+
 obj1 = readExcel()
-obj1.readExcel('1.xls')
+obj1.writeInfromToFile()
+obj1.writeExcelQuestions('1.xls')
+obj1.writeOtherInf('1.xls')
