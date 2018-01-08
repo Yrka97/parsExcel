28bdcf07bc15081b054a99df5f2c63da5492d50e
Cartmanidze <glebon84@gmail.com>
2018-01-08 14:32:50 +0600

    pars
    Signed-off-by: Cartmanidze <glebon84@gmail.com>

--- venv/writeExcel.py
+++ venv/writeExcel.py
@@ -1,19 +1,27 @@
 import xlrd
 class readExcel:
+
     def readExcel(self,nameFileExcel):
         try:
             rb = xlrd.open_workbook(nameFileExcel)
         except FileNotFoundError:
-            print('Ошибка-'+FileNotFoundError)
+            print('Ошибка файл не найден')
         sheet = rb.sheet_by_index(0)
         for rownum in range(sheet.nrows):
             row = sheet.row_values(rownum)
-            print(row)
-    def createFile(self,nameFile):
+            stringRow = str(row)
+            obj1.writeFile('some.txt',stringRow)
+            print(stringRow)
+
+    def writeFile(self,nameFile,write):
         try:
-            my_file = open(nameFile,'w')
-        except FileExistsError:
-           print('Ошибка-'+FileExistsError)
+            my_file = open(nameFile,'a')
+        except FileNotFoundError:
+           print('Ошибка файл не найден')
+        try:
+            my_file.write(write)
+        except IOError:
+            print('Ошибка записи в файл')
+
 obj1 = readExcel()
 obj1.readExcel('1.xls')
-obj1.createFile('some.txt')
