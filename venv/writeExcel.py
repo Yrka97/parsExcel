447017b2fdf57f596000d4d9894495d04781490c
Cartmanidze <glebon84@gmail.com>
2018-01-08 14:00:08 +0600

    create file
    Signed-off-by: Cartmanidze <glebon84@gmail.com>

--- venv/writeExcel.py
+++ venv/writeExcel.py
@@ -1,6 +1,13 @@
 import xlrd
-rb = xlrd.open_workbook('1.xls')
-sheet = rb.sheet_by_index(0)
-for rownum in range(sheet.nrows):
-    row = sheet.row_values(rownum)
-    print(row)
+class readExcel:
+    def readExcel(self,nameFileExcel):
+        rb = xlrd.open_workbook(nameFile)
+        sheet = rb.sheet_by_index(0)
+        for rownum in range(sheet.nrows):
+            row = sheet.row_values(rownum)
+            print(row)
+    def createFile(self,nameFile):
+        my_file = open(nameFile,wr)
+        my_file.fileno()
+obj1 = readExcel()
+obj1.readExcel('1.xls')
